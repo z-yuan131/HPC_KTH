dacf267e8ed37df10737b0bc98ea4cf5bf478d0c
z-yuan131 <zhenyang@kth.se>
2022-04-18 23:07:30 +0200

    q4 finalized

--- assignment2/q1/hello_world_openmp.c
+++ assignment2/q1/hello_world_openmp.c
@@ -2,7 +2,7 @@
 #include <omp.h>
 
 int main(int argc, char *argv[]){
-  omp_set_num_threads(11);  //default is 8 on my mac. I assume each core a thread?
+  omp_set_num_threads(4);  //default is 8 on my mac. I assume each core a thread?
   #pragma omp parallel
   {
     int id = omp_get_thread_num();
