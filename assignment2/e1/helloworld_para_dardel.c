31803b605eba8bab9fc6b839fd26e52886384789
Ke <kezhang@kth.se>
2022-05-11 23:42:16 +0200

    new bash tested

--- assignment2/e1/helloworld_para_dardel.c
+++ assignment2/e1/helloworld_para_dardel.c
@@ -2,7 +2,7 @@
 #include <omp.h>
 
 int main (){
-	omp_set_num_threads(4);
+	//omp_set_num_threads(4);
 	#pragma omp parallel
 	{
 	int id=omp_get_thread_num();
--- assignment2/e1/myjob.sh
+++ assignment2/e1/myjob.sh
@@ -1,3 +1,19 @@
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+# The name of the script is myjob
+#SBATCH -J myjob
+# Only 1 hour wall-clock time will be given to this job
+#SBATCH -t 00:01:00
+#SBATCH -A edu22.DD2356
+# Number of nodes
+#SBATCH -p shared
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --nodes=1
+#SBATCH -e error_file.e
+
+export OMP_NUM_THREADS=8
 cc -fopenmp -o helloworld_para_dardel.out helloworld_para_dardel.c 
-srun -n 1 ./helloworld_para_dardel.out > hello_output
+srun -c 8 ./helloworld_para_dardel.out > hello_output
 cat hello_output
