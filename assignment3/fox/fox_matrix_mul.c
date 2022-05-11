a669b8f3d26d46fc3dbaaa684e4af48365871ef7
z-yuan131 <zhenyang@kth.se>
2022-05-11 14:14:50 +0200

    fox finished

--- assignment3/fox/fox_matrix_mul.c
+++ assignment3/fox/fox_matrix_mul.c
@@ -6,44 +6,85 @@
 #include <math.h>
 #include <time.h>
 
-#define MSIZE 5000    //1024, a num which is pow of 2
+#define MSIZE 1024    //1024, a num which is pow of 2
+
+#define SEED     921
 
 #define Trials 10
 
 double A[MSIZE][MSIZE];
 double B[MSIZE][MSIZE];
-double A_buff[MSIZE][MSIZE];
-double B_buff[MSIZE][MSIZE];
 double C[MSIZE][MSIZE];
-double temp[MSIZE][MSIZE];
+
+// a function to allocate 2d arrays
+double **alloc_2d_array(int rows, int cols) {
+  int i;
+  // first make a one dimentional array
+  double *data = (double *)malloc(rows*cols*sizeof(double));
+  // allocate each row to a new pointer
+  double **array= (double **)malloc(rows*sizeof(double*));
+  for (i=0; i<rows; i++)
+    array[i] = &(data[cols*i]);
+
+  return array;
+}
+
+double** Generate(int rows, int cols) {
+    double** array = (double**) malloc(sizeof(double[cols]) * rows);
+
+    for(int i = 0; i < rows; i++) {
+        array[i] = malloc(sizeof(double) * cols);
+        //for(int j = 0; j < cols; j++) {
+        //    array[i][j] = 1;
+        //}
+    }
+
+    return array;
+}
 
 
-void initialize_matrices(int Nsize)
+void initialize_matrices(int Nsize, double **A_local, double **B_local, double **C_local, double **temp, int * coord)
 {
   int i, j;
+  srand(SEED);
 
-  for (i = 0 ; i < Nsize ; i++) {
-    for (j = 0 ; j < Nsize ; j++) {
+  for (i = 0 ; i < MSIZE ; i++) {
+    for (j = 0 ; j < MSIZE ; j++) {
       A[i][j] = (double) rand() / RAND_MAX;
       B[i][j] = (double) rand() / RAND_MAX;
       C[i][j] = 0.0;
-      temp[i][j] = 0.0;
       //printf("%f,%f\n", A[i][j], B[i][j]);
+    }
+  }
+
+  for (i = 0; i < Nsize; i++){
+    for (j = 0; j < Nsize; j++){
+      A_local[i][j] = A[i + coord[0]*Nsize][j + coord[1]*Nsize];
+      B_local[i][j] = B[i + coord[0]*Nsize][j + coord[1]*Nsize];
+      C_local[i][j] = 0.0;
+      temp[i][j] = 0.0;
     }
   }
 }
 
-void boardcast_matrices(int Nsize, int * dim, MPI_Comm row_comm,MPI_Comm comm_cart, int rank, int row_rank, int i, int y_source, int y_dest){
+void boardcast_matrices(int Nsize, int * dim, MPI_Comm row_comm,MPI_Comm comm_cart, int rank, int row_rank, int i, int y_source, int y_dest, double **A_local, double **B_local, double **A_buff, double **B_buff){
   //printf("%d,%d,%d\n", Nsize,dim[0],dim[1]);
 
   int root_rank;
 
-  // initialize A_buff with root_rank
+  // initialize A_buff with local varibles
   for (int j = 0; j < Nsize; j++){
     for (int k = 0; k < Nsize; k++){
-      A_buff[j][k] = A[j][k];
+      A_buff[j][k] = A_local[j][k];
+      if (i == 0){
+        B_buff[j][k] = B_local[j][k];
+      }
+      else{
+        B_local[j][k] = B_buff[j][k];     //update B_local
+      }
     }
   }
+
 
   //int MPI_Bcast(void* buffer,
   //          int count,
@@ -65,7 +106,8 @@ void boardcast_matrices(int Nsize, int * dim, MPI_Comm row_comm,MPI_Comm comm_ca
   }
 
   // boardcast A in row direction
-  MPI_Bcast(&A_buff, (int)(Nsize*Nsize), MPI_DOUBLE, root_rank, row_comm);
+  MPI_Bcast(&(A_buff[0][0]), (int)(Nsize*Nsize), MPI_DOUBLE, root_rank, row_comm);
+
 
   // get B from y direction neighbours
   //MPI_Sendrecv(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
@@ -75,23 +117,25 @@ void boardcast_matrices(int Nsize, int * dim, MPI_Comm row_comm,MPI_Comm comm_ca
 
   // at the first iteration, this opration is not needed.
   if (i != 0){
-    MPI_Sendrecv(&B,      (int)(Nsize*Nsize), MPI_DOUBLE, y_source,   1,
-                 &B_buff, (int)(Nsize*Nsize), MPI_DOUBLE, y_dest,     1, comm_cart, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&(B_local[0][0]), (int)(Nsize*Nsize), MPI_DOUBLE, y_source,   1,
+                 &(B_buff[0][0]),  (int)(Nsize*Nsize), MPI_DOUBLE, y_dest,     1, comm_cart, MPI_STATUS_IGNORE);
   }
 }
 
 
-void calculate_C(int Nsize)
+void calculate_C(int Nsize, double **C_local, double **A_buff, double **B_buff)
 {
     for (int i = 0; i < Nsize; i++){
       for (int j = 0; j < Nsize; j++) {
-          C[i][j] += A[i][j]*B[i][j];
+        for (int k = 0 ; k < Nsize ; k++) {
+          C_local[i][j] += A_buff[i][k]*B_buff[k][j];
+        }
       }
     }
 }
 
 
-void average_result(int Nsize, int rank, int size){
+void average_result(int Nsize, int rank, int size, double **C_local, double **temp){
   //int MPI_Reduce(const void* send_buffer,
   //             void* receive_buffer,
   //             int count,
@@ -99,19 +143,44 @@ void average_result(int Nsize, int rank, int size){
   //             MPI_Op operation,
   //             int root,
   //             MPI_Comm communicator);
-  MPI_Reduce(&C, &temp, (int)(Nsize*Nsize), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&(C_local[0][0]), &(temp[0][0]), (int)(Nsize*Nsize), MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-  double avg = 0;
+  int Rsize = (int)(Nsize * sqrt(size));
+  double avg = 0.0;
   if (rank == 0){
     for (int i = 0; i < Nsize; i++){
       for (int j = 0; j < Nsize; j++){
-        avg += temp[i][j] / (double)(Nsize*Nsize);
+        avg += temp[i][j];
       }
     }
-    avg = avg / size;
-    printf("summation is %f\n", avg);
+
+    printf("parallel summation result is %f\n", avg/(double)(Rsize*Rsize)/Trials);
   }
 }
+
+void average_result_for_serial_code(int Nsize, int size){
+  int Rsize = (int)(Nsize * sqrt(size));
+  printf("%d\n",Rsize);
+
+  double avg = 0.0;
+
+  for (int trial = 0; trial < Trials; trial++){
+    for (int i = 0 ; i < Rsize ; i++) {
+      for (int j = 0 ; j < Rsize ; j++) {
+        for (int k = 0 ; k < Rsize ; k++) {
+          avg += A[i][k] * B[k][j];
+        }
+      }
+    }
+  }
+
+
+  printf("serial summation result is %f \n", avg/(double)(Rsize*Rsize)/Trials);
+
+}
+
+
+
 
 int main(int argc, char* argv[]){
   int rank, size, i, provided;
@@ -190,17 +259,32 @@ int main(int argc, char* argv[]){
 
   //printf("1. Initializing Matrices \n");
   // init matrices with random numbers
+
   Nsize = (int)(MSIZE/(int)(sqrt(size)));
 
+  // define A_local,B_local,C_local,A_buff,B_buff and reduction matrix temp
 
-  initialize_matrices(Nsize);
+  double **A_local = Generate(Nsize,Nsize);
+  double **B_local = Generate(Nsize,Nsize);
+  double **C_local = Generate(Nsize,Nsize);
+  double **A_buff = Generate(Nsize,Nsize);
+  double **B_buff = Generate(Nsize,Nsize);
+  double **temp = Generate(Nsize,Nsize);
+
+
+
+  initialize_matrices(Nsize, A_local, B_local, C_local, temp, coord);
+
+
 
   // main solver
   for (int trial = 0; trial < Trials; trial++){
     for (int i = 0; i < dim[1]; i++){
-      boardcast_matrices(Nsize,dim,row_comm,comm_cart,rank,row_rank,i,y_source[0],y_dest[0]);
-      calculate_C(Nsize);
+      boardcast_matrices(Nsize,dim,row_comm,comm_cart,rank,row_rank,i,y_source[0],y_dest[0],A_local,B_local,A_buff,B_buff);
+
+      calculate_C(Nsize, C_local, A_buff, B_buff);
     }
+
   }
 
 
@@ -212,12 +296,18 @@ int main(int argc, char* argv[]){
   stop_time = MPI_Wtime();
   elapsed_time = stop_time - start_time;
 
-  average_result(Nsize, rank, size);
+  average_result(Nsize, rank, size, C_local, temp);
+
 
 
   if (rank == 0)
   {
-    printf("Final elapsed time is %f s\n", elapsed_time/(double)Trials);
+    start_time = MPI_Wtime();
+    average_result_for_serial_code(Nsize, size);
+    stop_time = MPI_Wtime();
+
+    printf("Final elapsed time for parallel job is %f s\n", elapsed_time/(double)Trials);
+    printf("Final elapsed time for serial job is %f s\n", (stop_time - start_time)/(double)Trials);
   }
 
 
